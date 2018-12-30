c56f7ee10484cb18b0fd070fd26cc2226ff2cccf
mycomycul <32719514+mycomycul@users.noreply.github.com>
2018-12-30 14:58:02 -0800

    Add delete functionality

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -2,13 +2,36 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.value,
+    id: this.props.counterId
   };
 
   render() {
-    return <div>{this.renderTags()}</div>;
+    console.log(this.props);
+
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={() => this.handleIncrement({ id: this.state.id })}
+          className="btn btn-secondary"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
   }
+  //Event Handlers
+  handleIncrement = id => {
+    console.log(id);
+    this.setState({ value: this.state.value + 1 });
+  };
 
   renderTags() {
     if (this.state.tags.length === 0) return <p>There are no tags</p>;
@@ -22,12 +45,12 @@ class Counter extends Component {
   }
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
